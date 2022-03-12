2c173cf6a3ca6cefdd4c7ea7aae19f064813bc19
Dimas <Dimas@DIMAS-PC>
2022-03-12 09:19:53 -0600

    Agregar archivos de proyecto.

---
+++ ADSProject/Models/EstudianteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ADSProject.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSProject.Models
+{
+    public class EstudianteViewModel
+    {
+        public int idEstudiante { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [StringLength(50,MinimumLength = 3, ErrorMessage = "La longitud del campo no debe ser mayor a 50 caracteres ni menor de 3 caracteres.")]
+        public string nombresEstudiante { get; set; }
+
+        public string apellidosEstudiante { get; set; }
+
+        public string codigoEstudiante { get; set; }
+
+        public string correoEstudiante { get; set; }
+    }
+}
