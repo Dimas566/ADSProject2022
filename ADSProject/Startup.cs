f161cda3adc540753852d13b271b9df2d5b84c66
Dimas <josedimasgarcia22@gmail.com>
2022-04-23 07:49:59 -0700

    Agregue configuracion para SQLServer

--- ADSProject/Startup.cs
+++ ADSProject/Startup.cs
@@ -1,6 +1,8 @@
+using ADSProject.Data;
 using ADSProject.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +27,13 @@ namespace ADSProject
         {
             services.AddControllersWithViews();
 
+
             // Inyeccion de dependencias
             services.AddSingleton<IEstudianteRepository, EstudianteRepository>();
+
+            //Configuracion de DB
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
