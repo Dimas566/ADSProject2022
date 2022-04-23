da68a58821e4440329fa58e73311afa75a1c6f61
Dimas <josedimasgarcia22@gmail.com>
2022-04-23 07:51:28 -0700

    Agregue la clase ApplicationDbContext dentro de la carpeta Data

---
+++ ADSProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ADSProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADSProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<EstudianteViewModel> Estudiantes { get; set; }
+    }
+}
